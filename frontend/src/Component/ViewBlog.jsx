24e03766b3d55e104ef0b33a5838884be568accc
Shyam3050 <nimmanashyam3050@gmail.com>
2024-08-23 16:25:07 +0530

    solved error

--- frontend/src/Component/ViewBlog.jsx
+++ frontend/src/Component/ViewBlog.jsx
@@ -62,7 +62,7 @@ const ViewBlog = (props) => {
 
           <Box>
             <Image
-              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              src={blog.image}
               alt="Green double couch with wooden legs"
               borderRadius="lg"
               w={"100%"}
@@ -70,21 +70,9 @@ const ViewBlog = (props) => {
             />
           </Box>
 
-          <Heading size="lg">Blog Details</Heading>
+          <Heading size="lg">{blog.title}</Heading>
           <Text color={"gray.500"} textAlign={"left"}>
-            your gateway to a world of diverse knowledge and insights. At Vega6
-            Horizon, we explore the vast expanse of topics, from cutting-edge
-            technology and business trends to lifestyle, travel, and personal
-            development. your gateway to a world of diverse knowledge and
-            insights. At Vega6 Horizon, we explore the vast expanse of topics,
-            from cutting-edge technology and business trends to lifestyle,
-            travel, and personal development. your gateway to a world of diverse
-            knowledge and insights. At Vega6 Horizon, we explore the vast
-            expanse of topics, from cutting-edge technology and business trends
-            to lifestyle, travel, and personal development. your gateway to a
-            world of diverse knowledge and insights. At Vega6 Horizon, we
-            explore the vast expanse of topics, from cutting-edge technology and
-            business trends to lifestyle, travel, and personal development.
+            {blog.description}
           </Text>
         </Stack>
       </Container>
